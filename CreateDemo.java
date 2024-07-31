ad953074131426fc98920f6e8c1b082d4755cf05
shaiksamreem <ss1374985@gmail.com>
2024-07-31 17:33:01 +0530

    Create CreateDemo.java

---
+++ CreateDemo.java
@@ -0,0 +1,27 @@
+package com.samreen.jdbc;
+	import java.sql.*;
+	public class JdbcDemo {
+
+			public static void main(String[] args) throws Exception {
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					}
+					catch(ClassNotFoundException e) {}
+			
+				
+				String sql_query = "create table employee_info(eno int primary key auto_increment , first_name varchar(50) , last_name varchar(50), age int);";
+				
+				
+				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbccruddb","root","root");
+				
+				Statement st = con.createStatement();
+			
+				st.executeUpdate(sql_query);
+	
+				System.out.println("Table created successfully");
+				con.close();
+
+				
+			}
+
+		}
